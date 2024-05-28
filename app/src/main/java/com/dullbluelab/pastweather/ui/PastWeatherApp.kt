85661f47321b51b652f07c21f237ddb0e23bc006
DullBlueLab <142051314+DullBlueLab@users.noreply.github.com>
2024-05-28 09:04:04 +0900

    Added clear data process

--- app/src/main/java/com/dullbluelab/pastweather/ui/PastWeatherApp.kt
+++ app/src/main/java/com/dullbluelab/pastweather/ui/PastWeatherApp.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
